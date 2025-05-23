0f00026d11726ddea51ae53f4980982825ce21a0
Luiz <luiz.rodrigues@scalait.com>
2025-05-22 21:03:17 -0300

    PlugTime-feature/ criacao de endpoints para listar todos os usuarios de maneira completa(admin) e maneira publica

--- src/main/java/com/luiz/plugtime/controller/CustomerController.java
+++ src/main/java/com/luiz/plugtime/controller/CustomerController.java
@@ -5,20 +5,20 @@ import com.luiz.plugtime.model.Customer;
 import com.luiz.plugtime.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
-    private CustomerService service;
+    private final CustomerService service;
 
     public CustomerController(CustomerService service) {
         this.service = service;
     }
 
+    // Creates a new Customer
     @PostMapping
     public ResponseEntity<CustomerDto> createCustomer(@RequestBody Customer customer){
         Customer savedCustomer = service.createCustomer(customer);
@@ -28,5 +28,17 @@ public class CustomerController {
                 savedCustomer.getEmail()
         );
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    // Retrieves the list containing public information about customers
+    @GetMapping("/public")
+    public ResponseEntity<List<CustomerDto>> getAllCustomersPublicInfo(){
+        return ResponseEntity.status(200).body(service.getAllCustomersPublicInfo());
+    }
+
+    // Retrieves the list containing private information about customers
+    @GetMapping("/admin")
+    public ResponseEntity<List<Customer>> getAllCustomersFull(){
+        return ResponseEntity.status(200).body(service.getAllCustomersFull());
     }
 }
--- src/main/java/com/luiz/plugtime/service/CustomerService.java
+++ src/main/java/com/luiz/plugtime/service/CustomerService.java
@@ -1,18 +1,48 @@
 package com.luiz.plugtime.service;
 
+import com.luiz.plugtime.dto.CustomerDto;
+import com.luiz.plugtime.exceptions.CustomerCreationException;
+import com.luiz.plugtime.exceptions.CustomerException;
 import com.luiz.plugtime.model.Customer;
 import com.luiz.plugtime.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
-    private CustomerRepository repository;
+    private final CustomerRepository repository;
 
     public CustomerService (CustomerRepository repository){
         this.repository = repository;
     }
 
+    // Create Customer
     public Customer createCustomer(Customer customer){
-        return repository.save(customer);
+        try{
+            return repository.save(customer);
+        } catch (Exception e) {
+            throw new CustomerCreationException(e.getMessage());
+        }
+    }
+
+    // Return all customers info with restricted access
+    public List<Customer> getAllCustomersFull(){
+        try {
+            return repository.findAll();
+        } catch (Exception e) {
+            throw new CustomerException(e.getMessage());
+        }
+    }
+
+    // Return all customers public info with the DTO content
+    public List<CustomerDto> getAllCustomersPublicInfo(){
+        try {
+            return repository.findAll().stream()
+                    .map(c -> new CustomerDto(c.getName(), c.getPhone(), c.getEmail()))
+                    .toList();
+        } catch (Exception e) {
+            throw new CustomerException(e.getMessage());
+        }
     }
 }
